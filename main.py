5a0db98889b760230d4b8351999c1af056b660af
vmvarga <38255353+vmvarga@users.noreply.github.com>
2022-08-08 09:45:02 +0200

    Fixes, add multiple dtrack instances

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import requests
 from datetime import datetime
+import urllib3
+
+DEPENDENCY_TRACK_BASE_URLS = {
+ ("https:// /api/v1/",""),
+ ("https:// /api/v1/"," ")
+}
 
 DEPENDENCY_TRACK_BASE_URL = "https:// /api/v1/"
 DEPENDENCY_TRACK_API_KEY = ""
@@ -27,18 +33,21 @@ def resolve_source(vulnid):
     return source
 
 
-def findprojectuuid(comp, s):
-    r = s.get(f'{DEPENDENCY_TRACK_BASE_URL}component/{comp}')
+def findprojectuuid(comp, s, dtrackuri=DEPENDENCY_TRACK_BASE_URL):
+    r = s.get(f'{dtrackuri}component/{comp}', verify=False)
     resp = r.json()
     projuuid = resp['project']['uuid']
     projname = f"{resp['project']['name']} {resp['project']['version']}"
     return projuuid, projname
 
 
-def search_vuln(source, vulnid, s, purllist=None):
+def search_vuln(source, vulnid, s, purllist=None, dtrackuri=DEPENDENCY_TRACK_BASE_URL):
     if purllist is None:
         purllist = []
-    r = s.get(f'{DEPENDENCY_TRACK_BASE_URL}vulnerability/source/{source}/vuln/{vulnid}')
+    r = s.get(f'{dtrackuri}vulnerability/source/{source}/vuln/{vulnid}', verify=False)
+    if r.status_code == 404:
+        return None
+
     resp = r.json()
     found = []
     vulnuuid = resp['uuid']
@@ -46,11 +55,14 @@ def search_vuln(source, vulnid, s, purllist=None):
     if not vulnname:
         vulnname = resp.get('vulnId')
 
+    if not resp.get('components'):
+        return None
+
     for comp in resp['components']:
         prjuuid = comp['project'].get('uuid')
         prjname = f"{comp['project'].get('name')} {comp['project'].get('version')}"
         if not prjuuid:
-            prjuuid, prjname = findprojectuuid(comp['uuid'], s)
+            prjuuid, prjname = findprojectuuid(comp['uuid'], s, dtrackuri=dtrackuri)
         vulnobj = {
             'compuuid': comp['uuid'],
             'prjuuid': prjuuid,
@@ -66,24 +78,24 @@ def search_vuln(source, vulnid, s, purllist=None):
     return found
 
 
-def suppress_vuln(vuln, s, comment=None):
+def suppress_vuln(vuln, s, comment=None, dtrackuri=DEPENDENCY_TRACK_BASE_URL):
     data = {
      "project": vuln['prjuuid'],
      "component": vuln['compuuid'],
      "vulnerability": vuln['vulnuuid'],
      "analysisState": "NOT_AFFECTED",
-     "comment": f"DSeptic suppressed this for you. {comment}",
+     "comment": f"DSeptic suppressed this for you more info appsecurity@ingrammicro.com. {comment}",
      "suppressed": "true"
     }
     headers = {"Content-Type": "application/json"}
-    uri = f"{DEPENDENCY_TRACK_BASE_URL}analysis"
+    uri = f"{dtrackuri}analysis"
     r = s.put(uri, json=data, headers=headers, verify=False)
     return r.status_code
 
 
-def is_suppressed(vuln, s):
-    uri = f"{DEPENDENCY_TRACK_BASE_URL}analysis?component={vuln['compuuid']}&vulnerability={vuln['vulnuuid']}"
-    r = s.get(uri)
+def is_suppressed(vuln, s, dtrackuri=DEPENDENCY_TRACK_BASE_URL):
+    uri = f"{dtrackuri}analysis?component={vuln['compuuid']}&vulnerability={vuln['vulnuuid']}"
+    r = s.get(uri, verify=False)
     if len(r.text) == 0:
         return False
     else:
@@ -98,8 +110,7 @@ def is_suppressed_indb(vuln, db):
     else:
         return False
 
-
-if __name__ == '__main__':
+def suppressvulnindtrackinstance(dtrackuri, apikey):
     count = 0
     for vulnerability in read_wlist():
         vulnid = vulnerability['vulnid']
@@ -107,14 +118,16 @@ if __name__ == '__main__':
         source = resolve_source(vulnid)
         comment = vulnerability.get('comment')
         s = requests.Session()
-        s.headers.update({'X-Api-Key': DEPENDENCY_TRACK_API_KEY})
-        found = search_vuln(source, vulnid, s, purllist)
+        s.headers.update({'X-Api-Key': apikey})
+        found = search_vuln(source, vulnid, s, purllist, dtrackuri=dtrackuri)
+        if not found:
+            continue
 
         for vuln in found:
-            if is_suppressed(vuln, s):
+            if is_suppressed(vuln, s, dtrackuri=dtrackuri):
                 #print(f"[*] {vuln['vulnname']} in {vuln['prjname']} is already suppressed")
                 continue
-            status = suppress_vuln(vuln, s, comment)
+            status = suppress_vuln(vuln, s, comment, dtrackuri=dtrackuri)
             count += 1
             if status == 200:
                 now = datetime.now()
@@ -124,3 +137,9 @@ if __name__ == '__main__':
     now = datetime.now()
     dt = now.strftime("%d/%m/%Y %H:%M:%S")
     print(f"[{dt}] Suppressed {count} vulnerabilities")
+
+
+if __name__ == '__main__':
+    urllib3.disable_warnings()
+    for dtrackurikey in DEPENDENCY_TRACK_BASE_URLS:
+        suppressvulnindtrackinstance(dtrackurikey[0], dtrackurikey[1])
